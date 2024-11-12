f107a47c7484698d11cbadfeeb89afa40060b83b
bhagyashreebehara <bhagyashreebehara085@gmail.com>
2024-11-12 11:39:10 +0530

    HELLOOOO

--- main.js
+++ main.js
@@ -18,6 +18,19 @@ function filterSelection(category) {
         }
       }
     }
-  }
   
+  var tablinks = document.getElementsByClassName("tab-links");
+  var tabcontents = document.getElementsByClassName("tab-contents");
+  function opentab(tabname,element){
+      for( let tablink of tablinks){
+          tablink.classList.remove("active-link");
 
+      }
+      for( let tabcontent of tabcontents){
+          tabcontent.classList.remove("active-tab");
+          
+      }
+      element.classList.add("active-link");
+      document.getElementById(tabname).classList.add("active-tab");
+    }
+  } 
